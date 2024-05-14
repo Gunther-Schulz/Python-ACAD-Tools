e9e926f9ccd8a71620a666a59f9704ea34ffe8d7
Gunther Schulz <dev@guntherschulz.de>
2024-05-14 07:03:32 +0200

    add support for Gemarkung and Gemeinde

--- process.py
+++ process.py
@@ -13,6 +13,14 @@ def load_project_settings(project_name):
         return project
 
 
+def get_layer_info(project_settings, layer_name):
+    shapefile = next((layer['shapeFile']
+                     for layer in project_settings['layers'] if layer['name'] == layer_name), None)
+    label = next((layer['label'] for layer in project_settings['layers']
+                 if layer['name'] == layer_name), None)
+    return shapefile, label
+
+
 def load_template(project_settings):
     if project_settings['useTemplate']:
         doc = ezdxf.readfile(TEMPLATE_DXF)
@@ -48,16 +56,16 @@ def apply_conditional_buffering(source_geom, target_geom, distance):
     # TODO: If it crosses Geltungsbereich, increase distance by x. Try by using a combination of target_parcels and geltungsbereich
     source_geom['geometry'] = source_geom['geometry'].apply(
         lambda x: conditional_buffer(x, target_geom, distance))
-    return source_geom
+    return source_geom  # This now ensures it returns a GeoDataFrame
 
 
-def labeled_centroid_points(source_geom, label):
+def labeled_center_points(source_geom, label):
     # Use representative_point() to get a point within the polygon
     points_within = source_geom.representative_point()
     return gpd.GeoDataFrame(geometry=points_within, data={"label": source_geom[label]})
 
 
-def setup_textt_style(doc, text_style_name):
+def add_text_style(doc, text_style_name):
     if text_style_name not in doc.styles:
         doc.styles.new(name=text_style_name, dxfattribs={
                        'font': 'Arial.ttf', 'height': 0.1})
@@ -75,9 +83,9 @@ def add_text(msp, text, x, y, layer_name, style_name):
                  })
 
 
-def add_text_to_centroids(msp, labeled_centroid_points_df, layer_name, style_name=None):
-    if style_name is None:
-        style_name = layer_name
+def add_text_to_center(msp, labeled_centroid_points_df, layer_name, style_name=None):
+    if style_name is None or not msp.styles.has_entry(style_name):
+        style_name = "default"
     for index, row in labeled_centroid_points_df.iterrows():
         x, y = row['geometry'].coords[0]
         add_text(msp, row['label'], x, y, layer_name, style_name)
@@ -86,7 +94,7 @@ def add_text_to_centroids(msp, labeled_centroid_points_df, layer_name, style_nam
 # Add geometries to DXF
 
 
-def add_geometries_to_dxf(msp, geometries, layer_name, close=False):
+def add_geometries(msp, geometries, layer_name, close=False):
     for geom in geometries:
         if geom.geom_type == 'Polygon':
             points = [(x, y) for x, y in geom.exterior.coords]
@@ -121,31 +129,59 @@ def add_layer(doc, layer_name, color):
 def main():
     parcels = load_shapefile(PARCEL_SHAPEFILE)
     flur = load_shapefile(FLUR_SHAPEFILE)
+
     parcel_list = ["1", "2", "4", "6", "7"]
     flur_list = ["Flur 1"]
+
+    gemeinde = load_shapefile(GEMEINDE_SHAPEFILE)
+    gemarkung = load_shapefile(GEMARKUNG_SHAPEFILE)
+
     target_parcels = filter_parcels(parcels, flur, parcel_list, flur_list)
     # check if this actually works by switching sides
     flur = apply_conditional_buffering(flur, target_parcels, 2)
-    parcel_points = labeled_centroid_points(parcels, PARCEL_LABEL)
+    geltungsbereich = target_parcels['geometry'].unary_union
+
+    parcel_points = labeled_center_points(parcels, PARCEL_LABEL)
+    flur_points = labeled_center_points(flur, FLUR_LABEL)
+    gemeinde_points = labeled_center_points(gemeinde, GEMEINDE_LABEL)
+    gemarkung_points = labeled_center_points(gemarkung, GEMARKUNG_LABEL)
 
     if project_settings['useTemplate']:
         doc = load_template(project_settings)
     else:
         doc = ezdxf.new('R2010', setup=True)
 
+    # Add defaults
+    add_text_style(doc, 'default')
+
     # This is optional and only useful if we want to set some attributes to the layer. Otherwise defaults are used
     add_layer(doc, 'Flur', COLORS['Flur'])
+    add_layer(doc, 'Gemeinde', COLORS['Gemeinde'])
+    add_layer(doc, 'Gemarkung', COLORS['Gemarkung'])
     add_layer(doc, 'Parcel', COLORS['Parcel'])
-    add_layer(doc, 'Geltungsbereich', COLORS['Geltungsbereich'])
-    setup_textt_style(doc, 'Parcel Number')
+    add_layer(doc, 'Parcel Number', COLORS['Parcel'])
+    add_layer(doc, 'Flur Number', COLORS['Flur'])
+    # add_layer(doc, 'Gemeinde Name', COLORS['Gemeinde'])
+    # add_layer(doc, 'Gemarkung Name', COLORS['Gemarkung'])
+
+    add_layer(doc, 'Geltungsbereich', 10)
+    add_text_style(doc, 'Parcel Number')
+    add_text_style(doc, 'Flur Number')
+    add_text_style(doc, 'Gemeinde Name')
+    add_text_style(doc, 'Gemarkung Name')
 
     msp = doc.modelspace()
 
-    add_geometries_to_dxf(
-        msp, [target_parcels['geometry'].unary_union], 'Geltungsbereich', True)
-    add_geometries_to_dxf(msp, flur['geometry'], 'Flur', True)
-    add_geometries_to_dxf(msp, parcels['geometry'], 'Parcel', True)
-    add_text_to_centroids(msp, parcel_points, 'Parcel Number')
+    add_geometries(
+        msp, [geltungsbereich], 'Geltungsbereich', True)
+    add_geometries(msp, flur['geometry'], 'Flur', True)
+    add_geometries(msp, gemeinde['geometry'], 'Gemeinde', True)
+    add_geometries(msp, gemarkung['geometry'], 'Gemarkung', True)
+    add_geometries(msp, parcels['geometry'], 'Parcel', True)
+    add_text_to_center(msp, parcel_points, 'Parcel Number')
+    add_text_to_center(msp, flur_points, 'Flur Number')
+    add_text_to_center(msp, gemeinde_points, 'Gemeinde Name')
+    add_text_to_center(msp, gemarkung_points, 'Gemarkung Name')
 
     doc.saveas(DXF_FILENAME)
     print(f"Saved {DXF_FILENAME}")
@@ -160,14 +196,13 @@ if __name__ == "__main__":
         CRS = project_settings['crs']
         DXF_FILENAME = project_settings['dxfFilename']
         TEMPLATE_DXF = project_settings['template']
-        FLUR_SHAPEFILE = next(
-            (layer['shapeFile'] for layer in project_settings['layers'] if layer['name'] == "Flur"), None)
-        FLUR_LABEL = next(
-            (layer['label'] for layer in project_settings['layers'] if layer['name'] == "Flur"), None)
-        PARCEL_SHAPEFILE = next(
-            (layer['shapeFile'] for layer in project_settings['layers'] if layer['name'] == "Parcel"), None)
-        PARCEL_LABEL = next(
-            (layer['label'] for layer in project_settings['layers'] if layer['name'] == "Parcel"), None)
+        GEMEINDE_SHAPEFILE, GEMEINDE_LABEL = get_layer_info(
+            project_settings, "Gemeinde")
+        GEMARKUNG_SHAPEFILE, GEMARKUNG_LABEL = get_layer_info(
+            project_settings, "Gemarkung")
+        FLUR_SHAPEFILE, FLUR_LABEL = get_layer_info(project_settings, "Flur")
+        PARCEL_SHAPEFILE, PARCEL_LABEL = get_layer_info(
+            project_settings, "Parcel")
         COLORS = {layer['name']: layer['color']
                   for layer in project_settings['layers']}
     # Add your processing logic here
