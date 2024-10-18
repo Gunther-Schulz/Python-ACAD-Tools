2253f103b199124c2c8cad5890d43f84bc4eb75d
Gunther Schulz <dev@guntherschulz.de>
2024-10-18 11:18:07 +0200

    5

--- conver_aci_txt_to_csv.py
+++ conver_aci_txt_to_csv.py
@@ -1,41 +1,9 @@
 import csv
-import webcolors
 import yaml
-from math import sqrt
 from colorsys import rgb_to_hsv
 
 def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(rgb[0], rgb[1], rgb[2])
-
-def create_color_mapping():
-    color_mapping = {}
-    for name in webcolors.names():
-        try:
-            rgb = webcolors.name_to_rgb(name)
-            color_mapping[rgb] = name
-        except ValueError:
-            # Skip colors that might not be recognized
-            pass
-    return color_mapping
-
-COLOR_MAPPING = create_color_mapping()
-
-def get_color_distance(c1, c2):
-    # Weighted Euclidean distance, giving more weight to luminance differences
-    return sqrt(2 * (c1[0] - c2[0])**2 + 4 * (c1[1] - c2[1])**2 + 3 * (c1[2] - c2[2])**2)
-
-def detect_tint(rgb):
-    # Detect if the color has a noticeable tint
-    max_component = max(rgb)
-    min_component = min(rgb)
-    if max_component - min_component > 10:  # Lowered threshold for more sensitivity
-        if rgb[0] == max_component:
-            return "red"
-        elif rgb[1] == max_component:
-            return "green"
-        elif rgb[2] == max_component:
-            return "blue"
-    return None
 
 def closest_colour(requested_colour, used_names):
     r, g, b = requested_colour
@@ -129,26 +97,12 @@ def closest_colour(requested_colour, used_names):
     
     return name
 
-def get_colour_name(rgb_triplet, used_names, previous_colors):
-    hex_color = rgb_to_hex(rgb_triplet)
-    try:
-        name = webcolors.hex_to_name(hex_color)
-        if name in used_names:
-            return closest_colour(rgb_triplet, used_names)
-        return name
-    except ValueError:
-        return closest_colour(rgb_triplet, used_names)
+def get_colour_name(rgb_triplet, used_names):
+    return closest_colour(rgb_triplet, used_names)
 
 def convert_to_csv_css_and_yaml(input_file, output_csv, output_css, output_yaml):
     used_names = set()
     yaml_data = []
-    previous_colors = []
-    
-    # Add color name overrides
-    color_overrides = {
-        "dark-reddish-maroon-9": "black",
-        "very-dark-black": "very-darkgrey"
-    }
     
     with open(input_file, 'r') as infile, \
          open(output_csv, 'w', newline='') as outfile_csv, \
@@ -182,26 +136,16 @@ def convert_to_csv_css_and_yaml(input_file, output_csv, output_css, output_yaml)
             
             # Get color name and hex from Ermittelt RGB values
             rgb = tuple(map(int, parts[7:10]))
-            color_name = get_colour_name(rgb, used_names, previous_colors)
-            
-            # Apply color name override if it exists
-            original_color_name = color_name
-            color_name = color_overrides.get(color_name.replace(' ', '-').lower(), color_name)
+            color_name = get_colour_name(rgb, used_names)
             
             used_names.add(color_name)
-            hex_color = rgb_to_hex(rgb)
-            
-            # Update previous_colors
-            previous_colors.append(color_name)
-            if len(previous_colors) > 3:  # Keep only the last 3 colors
-                previous_colors.pop(0)
             
             # Write to CSV
-            csv_writer.writerow([aci, autodesk, berechnet, ermittelt, color_name, hex_color])
+            csv_writer.writerow([aci, autodesk, berechnet, ermittelt, color_name, rgb_to_hex(rgb)])
             
             # Write to CSS
             css_class_name = color_name.replace(' ', '-').lower()
-            outfile_css.write(f".{css_class_name} {{ color: {hex_color}; }} /* ACI: {aci} */\n")
+            outfile_css.write(f".{css_class_name} {{ color: {rgb_to_hex(rgb)}; }} /* ACI: {aci} */\n")
             
             # Prepare YAML data
             yaml_data.append({
