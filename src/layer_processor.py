1b33af7b30d57184b73143f0cc963ff38cd1fe71
Gunther Schulz <dev@guntherschulz.de>
2024-11-05 12:53:26 +0100

    add points type for geom layers. this is necessary for block placement

--- src/layer_processor.py
+++ src/layer_processor.py
@@ -73,7 +73,7 @@ class LayerProcessor:
             return
     
         # Check for unrecognized keys
-        recognized_keys = {'name', 'updateDxf', 'operations', 'shapeFile', 'dxfLayer', 'outputShapeFile', 'style', 'close', 'linetypeScale', 'linetypeGeneration', 'viewports', 'attributes', 'bluntAngles', 'label', 'applyHatch', 'plot'}
+        recognized_keys = {'name', 'updateDxf', 'operations', 'shapeFile', 'type','dxfLayer', 'outputShapeFile', 'style', 'close', 'linetypeScale', 'linetypeGeneration', 'viewports', 'attributes', 'bluntAngles', 'label', 'applyHatch', 'plot'}
         unrecognized_keys = set(layer_obj.keys()) - recognized_keys
         if unrecognized_keys:
             log_warning(f"Unrecognized keys in layer {layer_name}: {', '.join(unrecognized_keys)}")
