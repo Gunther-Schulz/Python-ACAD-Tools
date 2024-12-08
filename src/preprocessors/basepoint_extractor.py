bb573c0ed495435a2f10b80741bbed65ae5efe55
Gunther Schulz <dev@guntherschulz.de>
2024-12-08 15:30:14 +0100

    add text content to "label" column

--- src/preprocessors/basepoint_extractor.py
+++ src/preprocessors/basepoint_extractor.py
@@ -37,8 +37,36 @@ def extract_entity_basepoints(entities, layer_name):
                 basepoint = entity.dxf.center
             elif entity.dxftype() == 'TEXT':
                 basepoint = entity.dxf.insert
+                attributes['label'] = entity.dxf.text
+            elif entity.dxftype() == 'LWPOLYLINE':
+                # Use first vertex as basepoint for LWPOLYLINE
+                if len(entity.vertices()) > 0:
+                    basepoint = entity.vertices()[0]
+            elif entity.dxftype() == 'MTEXT':
+                basepoint = entity.dxf.insert
+                attributes['label'] = entity.text
             elif entity.dxftype() == 'POINT':
                 basepoint = entity.dxf.location
+            elif entity.dxftype() == 'ELLIPSE':
+                basepoint = entity.dxf.center
+            elif entity.dxftype() == 'SPLINE':
+                # Use first control point as basepoint
+                if len(entity.control_points) > 0:
+                    basepoint = entity.control_points[0]
+            elif entity.dxftype() == 'HATCH':
+                # Use the center of the bounding box as basepoint
+                bbox = entity.bbox()
+                if bbox:
+                    basepoint = ((bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2)
+            elif entity.dxftype() == '3DFACE':
+                # Use first vertex as basepoint
+                basepoint = entity.dxf.vtx0
+            elif entity.dxftype() == 'DIMENSION':
+                basepoint = entity.dxf.defpoint
+            elif entity.dxftype() == 'LEADER':
+                # Use first vertex as basepoint
+                if entity.vertices:
+                    basepoint = entity.vertices[0]
             
             if basepoint is not None:
                 basepoint_features.append({
